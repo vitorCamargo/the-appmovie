a258ea649c114f1f64cf0e2aaf83f09d7ff8ee82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-26 01:13:26 +0000

    Deploy vitorCamargo/the-appmovie to github.com/vitorCamargo/the-appmovie.git:gh-pages

---
+++ precache-manifest.4c755b147a5bd76d52390faa28e0862b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587bbf4bdcb00ed90fc462c0ace8ba74",
+    "url": "/the-appmovie/index.html"
+  },
+  {
+    "revision": "5365b1a6b5a786b227b6",
+    "url": "/the-appmovie/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "cf9bfeb4470b37aa2fcf",
+    "url": "/the-appmovie/static/css/main.e8a3aceb.chunk.css"
+  },
+  {
+    "revision": "5365b1a6b5a786b227b6",
+    "url": "/the-appmovie/static/js/2.acb5e4a0.chunk.js"
+  },
+  {
+    "revision": "cf9bfeb4470b37aa2fcf",
+    "url": "/the-appmovie/static/js/main.971c0119.chunk.js"
+  },
+  {
+    "revision": "93f0328b49a3845b0a45",
+    "url": "/the-appmovie/static/js/runtime-main.0d1b8118.js"
+  },
+  {
+    "revision": "db19f899f11b7067dd3ec0c130186eec",
+    "url": "/the-appmovie/static/media/background-highlight.db19f899.svg"
+  },
+  {
+    "revision": "eee05a189dcfebfa2777af0d46350a08",
+    "url": "/the-appmovie/static/media/close.eee05a18.svg"
+  },
+  {
+    "revision": "5e8e1e4cf265e9d02d6219a9f7a3eebb",
+    "url": "/the-appmovie/static/media/down.5e8e1e4c.svg"
+  },
+  {
+    "revision": "0aea48978ef8aae4afe84e7091bc35e4",
+    "url": "/the-appmovie/static/media/logo.0aea4897.svg"
+  },
+  {
+    "revision": "079e2806e2b2211c1f96243a1e186caf",
+    "url": "/the-appmovie/static/media/next-slide.079e2806.svg"
+  },
+  {
+    "revision": "6d49e9d5b24021327ced4a20b0a851fe",
+    "url": "/the-appmovie/static/media/play-white.6d49e9d5.svg"
+  },
+  {
+    "revision": "59bba5a6c4321abd126e53ade79a59e3",
+    "url": "/the-appmovie/static/media/search-black.59bba5a6.svg"
+  },
+  {
+    "revision": "0b10c22969959e5a9029db06c106aea7",
+    "url": "/the-appmovie/static/media/search-white.0b10c229.svg"
+  },
+  {
+    "revision": "013cdda46ff3e5f6e847bde1d94a0ad5",
+    "url": "/the-appmovie/static/media/see-more.013cdda4.svg"
+  }
+]);
